66f6ceacf96b82c0011f63509fc7905531b16e00
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-07-30 19:28:20 -0400

    Styling of title and items are finished

--- pantry-tracker/app/page.js
+++ pantry-tracker/app/page.js
@@ -1,29 +1,55 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
-const items = ['tomato','potato','onion','garlic','ginger','carrot']
+const items = ['tomato','potato','onion','garlic','ginger','carrot', 'lettuce', 'kale', 'cucumber']
 
 export default function Home() {
-  return <Box 
+  return (
+  <Box 
   width="100vw" 
   height="100vh"
   display={'flex'}
   justifyContent={'center'}
+  flexDirection={'column'}
   alignItems={'center'}
   >
-    <Stack width="800px" height="600px" spacing={2}>
+  <Box border={'1px solid #333'}>
+    <Box 
+    width='800px'
+    height='100px'
+    bgcolor={'light-blue'}
+    display={'flex'} 
+    justifyContent={'center'}
+    alignItems={'center'}
+    >
+      <Typography variant="h2" color={'#333'} textAlign='center'>
+        Pantry Items
+      </Typography>
+    </Box>
+    <Stack width="800px" height="300px" spacing={2} overflow={'auto'}>
       {items.map((i) => (
         <Box
           key = {i}
           width="100%"
-          height="100px"
+          height="300px"
           display={'flex'}
           justifyContent={'center'}
           alignItems={'center'}
-          bgcolor={'gray'}
+          bgcolor={'#f0f0f0'}
         >
-          {i}
+          <Typography
+            variant={'h3'}
+            color={'#333'}
+            textAlign={'center'}
+            >
+              {
+                //Capitlize the first letter of each item
+                i.charAt(0).toUpperCase() + i.slice(1)
+              }
+            </Typography>
         </Box>
       ))}
     </Stack>
   </Box>
+  </Box>
+  )
 }
